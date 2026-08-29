f89b4d9c61879ed8f2a5a2c43a6e645fc25d1bf9
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2026-08-30 00:12:05 +0530

    Frontend + Backend Connection (Reg page)

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Star, Activity, Settings } from 'lucide-react';
-import { LoginPage } from '../pages/LoginPage';
+import { LoginPage } from '../pages/loginpage';
 import { RegisterPage } from '../pages/RegisterPage';
 import { DashboardPage } from '../pages/DashboardPage';
 import { BoardPage } from '../pages/BoardPage';
